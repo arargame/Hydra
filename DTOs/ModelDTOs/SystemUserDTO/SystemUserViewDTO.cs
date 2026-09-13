6ad9c2ee6bf8056317870fd4f86fc801f7110b3f
koray arar <arargame@hotmail.com>
2026-09-13 06:41:40 +0300

    feat(dto): configure SystemUserViewDTO with granular view mappings and property ordering

--- DTOs/ModelDTOs/SystemUserDTO/SystemUserViewDTO.cs
+++ DTOs/ModelDTOs/SystemUserDTO/SystemUserViewDTO.cs
@@ -17,6 +17,8 @@ namespace Hydra.DTOs.ModelDTOs.SystemUserDTO
         public bool EmailConfirmed { get; set; }
         public bool PhoneNumberConfirmed { get; set; }
         public Guid? PhoneNumberId { get; set; } = null;
+        public string? PasswordHash { get; set; } = null;
+        public Guid? PasswordResetValidationToken { get; set; } = null;
 
         public SystemUserViewDTO()
         {
@@ -25,48 +27,76 @@ namespace Hydra.DTOs.ModelDTOs.SystemUserDTO
 
         public override DTO LoadConfigurations()
         {
-            // 1. Email
+            // 1. Email (ListView, CollectionView, DetailsView, EditView, CreateView)
             SetConfigurationsViaStringPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.Email!),
                 displayName: "Email",
-                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter))
+                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter)),
+                useToCreateCollectionViewConfiguration: true
             );
 
-            // 2. Name
+            // 2. Name (ListView, CollectionView, DetailsView, EditView, CreateView)
             SetConfigurationsViaStringPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.Name!),
                 displayName: "Name",
-                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter))
+                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter)),
+                useToCreateCollectionViewConfiguration: true
             );
 
-            // 3. Description
+            // 3. Description (ListView, DetailsView, EditView, CreateView) - CollectionView YOK
             SetConfigurationsViaStringPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.Description!),
                 displayName: "Description",
                 attributeToFilter: new AttributeToFilter(nameof(ContainsFilter)),
-                htmlElementTypeInCreationAndEdit: HtmlElementType.TextArea
+                htmlElementTypeInCreationAndEdit: HtmlElementType.TextArea,
+                useToCreateCollectionViewConfiguration: false
             );
 
-            // 4. NickName
+            // 4. NickName (ListView, CollectionView, DetailsView, EditView, CreateView)
             SetConfigurationsViaStringPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.NickName!),
                 displayName: "Nickname",
-                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter))
+                attributeToFilter: new AttributeToFilter(nameof(ContainsFilter)),
+                useToCreateCollectionViewConfiguration: true
             );
 
-            // 5. EmailConfirmed
-            SetConfigurationsViaBooleanPropertyInfo(
+            // 5. EmailConfirmed (ListView, CollectionView, DetailsView, EditView) - CreateView YOK
+            SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.EmailConfirmed),
+                configurations: new List<IConfiguration>
+                {
+                    new ListViewConfiguration(
+                        toFilter: new AttributeToFilter(nameof(EqualFilter)),
+                        toOrder: new AttributeToOrder(isOrderable: true),
+                        elementType: HtmlElementType.DropdownList)
+                        .AlsoUseToCreateCollectionViewConfiguration(),
+
+                    new EditViewConfiguration(elementType: HtmlElementType.DropdownList),
+
+                    new DetailsViewConfiguration()
+                },
                 displayName: "Email Confirmed"
             );
 
-            // 6. PhoneNumberConfirmed
-            SetConfigurationsViaBooleanPropertyInfo(
+            // 6. PhoneNumberConfirmed (ListView, CollectionView, DetailsView, EditView) - CreateView YOK
+            SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.PhoneNumberConfirmed),
+                configurations: new List<IConfiguration>
+                {
+                    new ListViewConfiguration(
+                        toFilter: new AttributeToFilter(nameof(EqualFilter)),
+                        toOrder: new AttributeToOrder(isOrderable: true),
+                        elementType: HtmlElementType.DropdownList)
+                        .AlsoUseToCreateCollectionViewConfiguration(),
+
+                    new EditViewConfiguration(elementType: HtmlElementType.DropdownList),
+
+                    new DetailsViewConfiguration()
+                },
                 displayName: "Phone Confirmed"
             );
 
-            // 7. PhoneNumberId
+            // 7. PhoneNumberId (ListView, CollectionView, DetailsView, EditView) - CreateView YOK
             SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.PhoneNumberId!),
                 configurations: new List<IConfiguration>
@@ -77,18 +107,32 @@ namespace Hydra.DTOs.ModelDTOs.SystemUserDTO
                         .AlsoUseToCreateCollectionViewConfiguration()
                         .AlsoUseToCreateLookupViewConfiguration(),
 
+                    new EditViewConfiguration(),
+
                     new DetailsViewConfiguration()
                 },
                 displayName: "Phone Number Id"
             );
 
-            // 8. IsActive
-            SetConfigurationsViaBooleanPropertyInfo(
+            // 8. IsActive (ListView, CollectionView, DetailsView, EditView) - CreateView YOK
+            SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.IsActive),
+                configurations: new List<IConfiguration>
+                {
+                    new ListViewConfiguration(
+                        toFilter: new AttributeToFilter(nameof(EqualFilter)),
+                        toOrder: new AttributeToOrder(isOrderable: true),
+                        elementType: HtmlElementType.DropdownList)
+                        .AlsoUseToCreateCollectionViewConfiguration(),
+
+                    new EditViewConfiguration(elementType: HtmlElementType.DropdownList),
+
+                    new DetailsViewConfiguration()
+                },
                 displayName: "Active"
             );
 
-            // 9. AddedDate
+            // 9. AddedDate (ListView, CollectionView, DetailsView) - CreateView/EditView YOK
             SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.AddedDate),
                 configurations: new List<IConfiguration>
@@ -103,7 +147,7 @@ namespace Hydra.DTOs.ModelDTOs.SystemUserDTO
                 displayName: "Added Date"
             );
 
-            // 10. ModifiedDate
+            // 10. ModifiedDate (ListView, CollectionView, DetailsView) - CreateView/EditView YOK
             SetConfigurationsViaPropertyInfo(
                 propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.ModifiedDate),
                 configurations: new List<IConfiguration>
@@ -115,6 +159,31 @@ namespace Hydra.DTOs.ModelDTOs.SystemUserDTO
                     new DetailsViewConfiguration()
                 },
                 displayName: "Modified Date"
+            );
+
+            // 11. PasswordHash (DetailsView) - Sadece DetailsView
+            SetConfigurationsViaPropertyInfo(
+                propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.PasswordHash!),
+                configurations: new List<IConfiguration>
+                {
+                    new DetailsViewConfiguration()
+                },
+                displayName: "Password Hash"
+            );
+
+            // 12. PasswordResetValidationToken (CollectionView, DetailsView) - ListView YOK
+            SetConfigurationsViaPropertyInfo(
+                propertyInfo: ReflectionHelper.GetPropertyOf<SystemUserViewDTO>(x => x.PasswordResetValidationToken!),
+                configurations: new List<IConfiguration>
+                {
+                    new ListViewConfiguration(
+                        toFilter: new AttributeToFilter(nameof(EqualFilter)),
+                        toOrder: new AttributeToOrder(isOrderable: true))
+                        .AlsoUseToCreateCollectionViewConfiguration(useToCreateCollectionViewConfiguration: true, thenRemoveThis: true),
+
+                    new DetailsViewConfiguration()
+                },
+                displayName: "Password Reset Token"
             );
 
             SortByWrittenOrder();
